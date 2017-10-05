aeb40acf86a3b170e7166a1e89702362ae7f01a1
Rahul Matta <rahulmatta2016@u.northwestern.edu>
2017-10-04 21:47:56 -0700

    gmaps hello world

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Nav from  './Nav';
+import MapContainer from './MapContainer';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Good Eggs Farmers</h2>
-        </div>
+      	<Nav/>
+      	<MapContainer />
       </div>
     );
   }
---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react';
+
+class Nav extends Component {
+  render() {
+    return (
+        <nav className="navbar navbar-inverse bg-inverse">
+        </nav>
+    );
+  }
+}
+
+export default Nav;
