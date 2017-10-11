c350f1c55fd184d68f4a595745d1500df99ca66a
Rahul Matta <rahulmatta2016@u.northwestern.edu>
2017-10-10 21:07:58 -0700

    correct farmer markers on click

--- src/components/MapContents.js
+++ src/components/MapContents.js
@@ -12,6 +12,7 @@ class MapContents extends Component {
         this.handlers = createHandlers(this.props.dispatch);
         this.state = {
         	"activeMarker": null,
+        	"activeFarmer": 0,
         	"selectedInfoWindow": false
         };
     }
@@ -20,35 +21,36 @@ class MapContents extends Component {
 		console.log(props,marker,e); 
 		this.setState({"activeMarker" : marker});
 		this.setState({"selectedInfoWindow": true});
+		this.setState({"activeFarmer": props.index});
 	}
 
 	onMapClick(props, map, e){
 		console.log(props, map, e);
 		this.setState({activeMarker : null});
 		this.setState({selectedInfoWindow: false});
+		this.setState({"activeFarmer": 0});
 	}
 
     render() {
+    	
     	if (this.props.products && this.props.farmers){
-			const farmerMarkers = this.props.farmers.slice(0,1).map(farmer => {
-				return (<Marker key={farmer["farmer_ID"]} name={farmer["farmer_name"]} position={{lat: farmer["farmer_lat"], lng: farmer["farmer_lng"]}} onClick={this.onMarkerClick} />);
+			let farmerMarkers = this.props.farmers.map(farmer => {
+				return (<Marker key={farmer["farmer_ID"]} id={farmer["farmer_ID"]} index={this.props.farmers.indexOf(farmer)} name={farmer["farmer_name"]} position={{lat: farmer["farmer_lat"], lng: farmer["farmer_lng"]}} onClick={this.onMarkerClick} />);
 			});
 			return (
 			  <Map google={window.google} clickableIcons={false} zoom={3} initialCenter={{lat: 16.815852,lng: -27.973152}} onClick={this.onMapClick}>
 			 	{farmerMarkers}
 			 	<InfoWindow marker={this.state.activeMarker} visible={this.state.selectedInfoWindow}>
 				    <div>
-				      <h1>test</h1>
+				      <h1>{this.props.farmers[this.state.activeFarmer]["farmer_ID"]}</h1>
 				    </div>
 				</InfoWindow>
 			  </Map>
 			)
 		}
 		else {
-				return (
-				  null
-				)
-			}
+				return (null)
+			 }
 	    }
 
     
